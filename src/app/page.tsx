12cb606fec93044cd4d4e28acf7af5bb640bc89c
WaxArsatia <waxarsatia@gmail.com>
2024-08-17 17:53:38 +0700

    fix: Add gap on mobile view
    Signed-off-by: WaxArsatia <waxarsatia@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -96,7 +96,7 @@ export default function Home() {
           className="bg-secondary py-12 md:py-24 lg:py-32 text-primary"
         >
           <div className="container mx-auto px-4 md:px-6">
-            <div className="grid gap-6 md:grid-cols-2 md:gap-12">
+            <div className="grid gap-12 md:grid-cols-2">
               <div className="flex justify-center">
                 <Image
                   src={'/undraw_about_us_page.svg'}
